01075166bc0587d89173255cba8ee2665ab41663
gufernandess <fernandesgustavogb@gmail.com>
2021-12-29 17:35:43 -0300

    Ajustes adicionais

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const bill = document.getElementById("bill").value;
+const numberOfPeople = document.getElementById("people").value;
+
+const tipPorcentage = document.querySelectorAll(".custom-button");
+
+console.log(tipPorcentage);
+
+tipPorcentage[0].addEventListener("click", () => {
+    console.log();
+});
